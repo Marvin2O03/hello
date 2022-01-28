f2620eb7e9751a8bbf6397334ddce5ef44cf367b
Marvin2O03 <morellana5@huskers.unl.edu>
2022-01-28 15:58:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *  Author: Marvin Orellana
+ *  Email: morellana5@huskers.unl.edu
+ *  Date: 2022/01/28
+ *
+ *  A simple program that prints out my name
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Marvin Orellana\n");
+
+     return 0;
+ }
